da48226e56490f3a8a5e26280ecf4d1a20464f85
Kevin Vanantwerpen <kevin.v1989@gmail.com>
2019-10-02 19:31:32 +0200

    added scripts

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/style.scss';
-
+import queryString from 'query-string';
 import Axios from 'axios';
 
-import queryString from 'query-string';
+// Access function of wehter 'overview' or 'detail'
+import { initOverview } from './overview.js';
+import { initDetail } from './detailpage.js';
 
 const parsed = queryString.parse(location.search);
 console.log(parsed);
-console.log('webpack starterkit');
 
+// Get URL to decide wich script to execute
+if(!parsed){
+  initOverview();
+}else{
+  initDetail();
+}
